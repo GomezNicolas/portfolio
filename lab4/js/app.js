668d58b078ad85e2668a0a91f7964cf0a4d6a2f2
Nicolas <nicolas.gomez.colchero@gmail.com>
2019-03-28 23:45:11 +0100

    added functions initialize and getMyLocation with lat and long

--- lab4/js/app.js
+++ lab4/js/app.js
@@ -4,5 +4,17 @@ class Weather{
         this.initialize();
     }
 
+    initialize(){
+        this.getMyLocation();
+    }
+    getMyLocation(){
+        navigator.geolocation.getCurrentPosition(position => {
+            let lat = position.coords.latitude;
+            let lng = position.coords.longitude;
+            this.getWeather(lat, lng);
+        }, err => {
+            console.log(err);
+        } );
+    }
 
 }
