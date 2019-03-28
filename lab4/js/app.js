edd27252aedef05ade8db2507e09c63d6de1b6ee
Nicolas <nicolas.gomez.colchero@gmail.com>
2019-03-28 23:47:01 +0100

    added function getWeather: Ajax call -> return in json

--- lab4/js/app.js
+++ lab4/js/app.js
@@ -17,4 +17,25 @@ class Weather{
         } );
     }
 
+    getWeather(lat, lng){
+        //AJAX API CALL
+        let url = `http://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/${this.API_KEY}/${lat},${lng}?units=si`;
+        fetch(url)
+        //Lukt fetch? return in json
+        .then(response => {
+            return response.json(); //proberen json dan return
+        })
+        .then(json => {
+            let summary = document.querySelector(".weather-description");
+            summary.innerHTML = `Current Weather: <span>${json.currently.summary}</span>`;
+            let temp = document.querySelector(".temperature");
+            let roundedTemp = Math.round(json.currently.temperature);
+            temp.innerHTML = `${roundedTemp} °C`;
+
+            let searchword = json.currently.icon;
+
+            let images = new Images('34c276038436e4cdc55315b27a951cc38e93e61231b430374e3f3664b7916afa', searchword);
+        })
+    }
+
 }
